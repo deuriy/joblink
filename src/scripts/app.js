9cccc7018ab5d2539c48eff99b541da6735f1545
Yuriy Demchenko <deuriy@yandex.ru>
2024-10-18 02:29:39 +0300

    Layout of the message Access restricted

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,3 +3,27 @@ import { Offcanvas, Popover, Collapse, Dropdown } from "bootstrap";
 import { Fancybox } from "@fancyapps/ui";
 
 export { Offcanvas /*Popover, Collapse, Dropdown, Fancybox, Application*/ };
+
+function setActiveMenuItem (menu) {
+  if (!menu) return;
+
+  let pageUrl = new URL(window.location.href);
+  let links = menu.querySelectorAll('a');
+  console.log(pageUrl.pathname);
+
+  for (let link of links) {
+    let linkHref = link.getAttribute('href');
+    console.log(linkHref);
+
+    if (pageUrl.pathname == linkHref) {
+      link.classList.add('active');
+    }
+  }
+}
+
+document.querySelectorAll('.user-menu').forEach(menu => {
+  console.log(menu);
+  setActiveMenuItem(menu);
+});
+
+
