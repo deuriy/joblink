f2dbcb628b056478c3808294eb0d6ef7411edb7c
Yuriy Demchenko <deuriy@yandex.ru>
2024-11-05 00:03:36 +0200

    Scipts editing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,18 +2,16 @@ import { Application } from "@hotwired/stimulus";
 import { Offcanvas, Popover, Collapse, Dropdown } from "bootstrap";
 import { Fancybox } from "@fancyapps/ui";
 
-export { Offcanvas /*Popover, Collapse, Dropdown, Fancybox, Application*/ };
+export { Offcanvas };
 
 function setActiveMenuItem (menu) {
   if (!menu) return;
 
   let pageUrl = new URL(window.location.href);
   let links = menu.querySelectorAll('a');
-  console.log(pageUrl.pathname);
 
   for (let link of links) {
     let linkHref = link.getAttribute('href');
-    console.log(linkHref);
 
     if (pageUrl.pathname == linkHref) {
       link.classList.add('active');
@@ -22,7 +20,6 @@ function setActiveMenuItem (menu) {
 }
 
 document.querySelectorAll('.user-menu').forEach(menu => {
-  console.log(menu);
   setActiveMenuItem(menu);
 });
 
